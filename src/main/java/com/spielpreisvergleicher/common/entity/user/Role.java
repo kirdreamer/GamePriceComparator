c667e95e72dac0b6d9dde03d923e9fd59311ed51
kirdreamer <3alog2@gmail.com>
2023-11-26 15:15:15 +0100

    Implement UserDetails for user - Updated User: - - Implemented interface UserDetails - - Added new Role for getAuthorities() - Added UserRepository for communication between Database and Application

---
+++ src/main/java/com/spielpreisvergleicher/common/entity/user/Role.java
@@ -0,0 +1,6 @@
+package com.spielpreisvergleicher.common.entity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/spielpreisvergleicher/common/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spielpreisvergleicher.common.repository;
+
+import com.spielpreisvergleicher.common.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
