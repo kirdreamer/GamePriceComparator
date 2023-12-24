5b7841b6d0cce5bf5db77d0bac8034ad3c04f62e
kirdreamer <3alog2@gmail.com>
2023-12-24 20:57:32 +0100

    EBE-58 Create Steam Impl for Request-Sender - Updated SteamGameRepository - Added reading SteamGame from database in GameService

--- src/main/java/com/spielpreisvergleicher/common/service/game/GameService.java
+++ src/main/java/com/spielpreisvergleicher/common/service/game/GameService.java
@@ -1,45 +1,33 @@
 package com.spielpreisvergleicher.common.service.game;
 
 import com.spielpreisvergleicher.common.dto.GogResponse;
-import com.spielpreisvergleicher.common.dto.SteamAllGamesResponse;
 import com.spielpreisvergleicher.common.entity.steam.SteamGame;
-import com.spielpreisvergleicher.common.mapper.SteamGameMapper;
-import com.spielpreisvergleicher.common.repository.SteamGameRepository;
 import com.spielpreisvergleicher.common.service.game.api.gog.impl.GameGetterGog;
-import com.spielpreisvergleicher.common.service.game.api.steam.impl.AllGamesGetterSteam;
 import com.spielpreisvergleicher.common.service.game.api.steam.impl.GameGetterSteam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class GameService {
-    private final SteamGameMapper steamGameMapper;
-    private final SteamGameRepository steamGameRepository;
     private final GameGetterSteam gameGetterSteam;
-    private final AllGamesGetterSteam allGamesGetterSteam;
     private final GameGetterGog gameGetterGog;
 
     public void getGamesByName(String name) { //TODO change to GameResponse
         GogResponse gogResponse = gameGetterGog.getGamesByName(name);
+
+        List<SteamGame> steamGames = gameGetterSteam.getGamesByName(name);
+        return;
         //TODO make SteamImpl
         //TODO combine common games
         //TODO create list Response for each found game
     }
 
-    public void getAllSteamGamesAndSaveIntoDatabase() {
-        List<SteamGame> steamGameList = new ArrayList<>();
-
-        for (SteamAllGamesResponse.AppList.App app : allGamesGetterSteam.getAllGames().applist().apps())
-            steamGameList.add(steamGameMapper.appToSteamGame(app));
-
-        log.info("Trying to save all received games from steam into database...");
-        steamGameRepository.saveAll(steamGameList);
-        log.info("All information was successfully saved");
+    public void getAllGamesAndSaveIntoDatabase() {
+        gameGetterSteam.getAllSteamGamesAndSaveIntoDatabase();
     }
 }
--- src/main/java/com/spielpreisvergleicher/common/service/game/api/steam/impl/GameGetterSteam.java
+++ src/main/java/com/spielpreisvergleicher/common/service/game/api/steam/impl/GameGetterSteam.java
@@ -1,17 +1,39 @@
 package com.spielpreisvergleicher.common.service.game.api.steam.impl;
 
-import org.springframework.context.annotation.Primary;
+import com.spielpreisvergleicher.common.dto.SteamAllGamesResponse;
+import com.spielpreisvergleicher.common.entity.steam.SteamGame;
+import com.spielpreisvergleicher.common.mapper.SteamGameMapper;
+import com.spielpreisvergleicher.common.repository.SteamGameRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+
 @Component
-@Primary
+@RequiredArgsConstructor
+@Slf4j
 public class GameGetterSteam {
+    private final SteamGameMapper steamGameMapper;
+    private final SteamGameRepository steamGameRepository;
+    private final AllGamesGetterSteam allGamesGetterSteam;
 
-    public void getGamesByName(String name) {
-
+    public List<SteamGame> getGamesByName(String name) {
+        return steamGameRepository.findByNameIgnoreCaseContaining(name).orElseGet(ArrayList::new);
     }
 
     public void getGameById(Integer id) {
 
+    }
+
+    public void getAllSteamGamesAndSaveIntoDatabase() {
+        Set<SteamGame> steamGameList = new HashSet<>();
+
+        for (SteamAllGamesResponse.AppList.App app : allGamesGetterSteam.getAllGames().applist().apps())
+            steamGameList.add(steamGameMapper.appToSteamGame(app));
+
+        log.info("Trying to save all received games from steam into database...");
+        steamGameRepository.saveAll(steamGameList);
+        log.info("All information was successfully saved");
     }
 }
