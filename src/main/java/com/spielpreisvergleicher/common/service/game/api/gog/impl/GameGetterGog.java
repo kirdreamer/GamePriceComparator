dd44e3d0effb25b9c137a8e69938934c29fdfd6a
kirdreamer <3alog2@gmail.com>
2023-12-25 20:28:40 +0100

    EBE-62 Rework gog API: Filter all games, which doesn't fulfill the name of the game - Updated Method getGamesByName in GameGetterGog: Now all games will be filtered.

--- src/main/java/com/spielpreisvergleicher/common/service/game/api/gog/impl/GameGetterGog.java
+++ src/main/java/com/spielpreisvergleicher/common/service/game/api/gog/impl/GameGetterGog.java
@@ -3,6 +3,7 @@ package com.spielpreisvergleicher.common.service.game.api.gog.impl;
 import com.spielpreisvergleicher.common.dto.GogProducts;
 import com.spielpreisvergleicher.common.dto.GogResponse;
 import com.spielpreisvergleicher.common.service.game.ExternalApiService;
+import io.jsonwebtoken.lang.Strings;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -24,6 +26,11 @@ public class GameGetterGog {
     public List<GogProducts> getGamesByName(String name) {
         String parameters = String.format("?search=%s", URLEncoder.encode(name, StandardCharsets.UTF_8));
         String finalUrl = String.format("%s%s", productsUrl, parameters);
-        return externalApiService.makeGetRequest(finalUrl, GogResponse.class).products();
+        return externalApiService.makeGetRequest(finalUrl, GogResponse.class).products().stream()
+                .filter(
+                        product -> Arrays.stream(Strings.delimitedListToStringArray(name, " "))
+                                .allMatch(product.title()::contains)
+                )
+                .toList();
     }
 }
