ed1098e0f2a26920002b5eef1b145a5a082c10e9
kirdreamer <3alog2@gmail.com>
2024-01-06 21:06:23 +0100

    EBE-60 Deleted unnecessary GET Method in GameController

--- src/main/java/com/gamepricecomparator/common/web/controller/GameController.java
+++ src/main/java/com/gamepricecomparator/common/web/controller/GameController.java
@@ -17,13 +17,6 @@ import java.util.List;
 public class GameController {
     private final GameService gameService;
 
-    @GetMapping("/get-all-games")
-    public ResponseEntity<Void> getAllGames() {
-        log.info("Received Request to get all games");
-        gameService.getAllGamesAndSaveIntoDatabase();
-        return ResponseEntity.ok().build();
-    }
-
     @GetMapping("/get-game-list")
     public ResponseEntity<List<GameResponse>> getGameList(@RequestParam("game") String nameGame) {
         log.info("Received Request to get a list of games with name {}", nameGame);
