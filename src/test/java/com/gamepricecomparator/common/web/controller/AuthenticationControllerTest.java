5b695d8772112d63a96b9d1c2ca6d885d72b4a54
kirdreamer <3alog2@gmail.com>
2024-02-12 15:59:52 +0100

    EBE-71 Write tests - The coverage of AuthenticationController tests by 100%

--- src/test/java/com/gamepricecomparator/common/web/controller/AuthenticationControllerTest.java
+++ src/test/java/com/gamepricecomparator/common/web/controller/AuthenticationControllerTest.java
@@ -3,6 +3,7 @@ package com.gamepricecomparator.common.web.controller;
 import com.gamepricecomparator.common.entity.user.User;
 import com.gamepricecomparator.common.web.request.AuthenticationRequest;
 import com.gamepricecomparator.common.web.request.RegisterRequest;
+import com.gamepricecomparator.common.web.response.IsLoggedInResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("integration-test")
@@ -61,7 +64,6 @@ class AuthenticationControllerTest extends AbstractBaseControllerTest {
             RegisterRequest registerRequest =
                     new RegisterRequest(DEFAULT_EMAIL, DEFAULT_NICKNAME, DEFAULT_PASSWORD);
 
-
             performPostRequestWithJson(REGISTER_URL, prepareRequestAsJsonString(registerRequest))
                     .andExpect(status().isConflict());
 
@@ -86,7 +88,6 @@ class AuthenticationControllerTest extends AbstractBaseControllerTest {
         }
     }
 
-
     @Nested
     @DisplayName("Login testing")
     class TestLogin {
@@ -104,7 +105,8 @@ class AuthenticationControllerTest extends AbstractBaseControllerTest {
             String loginResult = performPostRequestWithJson(LOGIN_URL, prepareRequestAsJsonString(authenticationRequest))
                     .andExpect(status().isOk())
                     .andReturn()
-                    .getResponse().getContentAsString();
+                    .getResponse()
+                    .getContentAsString();
 
             Mockito.verify(userRepository, times(2)).findByEmail(any(String.class));
 
@@ -143,7 +145,8 @@ class AuthenticationControllerTest extends AbstractBaseControllerTest {
             String loginResult = performPostRequestWithJson(LOGIN_URL, prepareRequestAsJsonString(authenticationRequest))
                     .andExpect(status().isBadRequest())
                     .andReturn()
-                    .getResponse().getContentAsString();
+                    .getResponse()
+                    .getContentAsString();
 
             Mockito.verify(userRepository, times(2)).findByEmail(any(String.class));
 
@@ -152,9 +155,53 @@ class AuthenticationControllerTest extends AbstractBaseControllerTest {
                     .isNotNull().hasToString("\"Email or Password is incorrect\"");
         }
     }
-//
-//    @Test
-//    public void testGetLoggedInUser() {
-//        log.info("bla");
-//    }
+
+    @Nested
+    @DisplayName("Token validation testing")
+    class TestLoggedInUser {
+        @Test
+        @DisplayName("(Positive) Should return response body with valid token")
+        void testGetLoggedInUser() throws Exception {
+            User user =
+                    buildMockUser(DEFAULT_ID, DEFAULT_EMAIL, DEFAULT_NICKNAME, DEFAULT_PASSWORD);
+
+            when(userRepository.findByEmail(DEFAULT_EMAIL)).thenReturn(Optional.ofNullable(user));
+
+            AuthenticationRequest authenticationRequest =
+                    new AuthenticationRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD);
+
+            String loginResult = performPostRequestWithJson(LOGIN_URL, prepareRequestAsJsonString(authenticationRequest))
+                    .andExpect(status().isOk())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+            Mockito.verify(userRepository, times(2)).findByEmail(any(String.class));
+
+            String token = objectMapper.readTree(loginResult).findValue("token").toString().replaceAll("\"", "");
+
+            assertThat(token).isNotNull();
+
+            String loggedInResult = mvc.perform(get(LOGGED_USER_URL)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                    .andExpect(status().isOk())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+            IsLoggedInResponse isLoggedInResponse = objectMapper.readValue(loggedInResult, IsLoggedInResponse.class);
+
+            assertThat(isLoggedInResponse.success()).isTrue();
+        }
+
+        @Test
+        @DisplayName("(Negative) Should reject login because of non existing user")
+        void testAttemptToValidateTokenWithWrongToken() throws Exception {
+            String wrongToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0ZXN0MUBleGFtcGxlLmNvbSIsImlhdCI6MTcwNzY0OTQyOCwiZXhwIjoxNzA3NjQ5NzI4fQ.ueSCRzp4_1BzWvpNtVf36h7BaM2usDlSzD5ctJN3HybAiZ_NPXgNWspibNkVVxpx";
+
+            mvc.perform(get(LOGGED_USER_URL)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer " + wrongToken))
+                    .andExpect(status().isForbidden());
+        }
+    }
 }
